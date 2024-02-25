33001de35482b0be8d4911c3b9e03c803fb4ad82
Vivek Pandagre <133694720+vap-pa@users.noreply.github.com>
2024-02-25 21:30:15 +0530

    Delete src directory

--- src/main/java/com/example/demo/Repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.Bean.BookCourse;
-
-public interface BookRepository extends CrudRepository<BookCourse, String> {
-
-}
